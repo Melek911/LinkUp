92b771e60949584b05e491d845122d8714187b2e
Melek911 <malekabderrahim19@gmail.com>
2024-12-25 12:45:33 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+// JavaScript لتفاعل الزر أو أحداث أخرى يمكن إضافتها حسب الحاجة
+document.querySelector('.cta-button').addEventListener('click', function() {
+    alert('مرحبًا بك في موقعنا! انضم الآن!');
+});
